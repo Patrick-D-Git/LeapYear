840c737f2c688a969ba3e275399cd60e3150e66e
Patrick D <patrick.montejo@gmail.com>
2024-01-28 15:15:24 -0500

    added comment that explains the leap year formula

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ if year % 4 == 0:
         print("Leap Year")
 else:
     print("Not Leap Year")
+
+
+# """ a year is a leap year if it is divisible by 4, except for years that are divisible by 100, unless they are
+# also divisible by 400. This formula ensures that the Gregorian calendar remains aligned with Earth's orbit around
+# the Sun as accurately as possible. """
